584b08869ab67dda450a6b1bc49f767cc1373691
DeLuks2006 <dragojeviclk16@gmail.com>
2024-06-26 16:16:36 +0000

    Add C2 Client (Basic)

--- client/src/main.go
+++ client/src/main.go
@@ -1,7 +1,136 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "io/ioutil"
+  "os/exec"
+  "os"
+  "bytes"
+  "encoding/json"
+)
+
+func clears() {
+  cmd := exec.Command("clear")
+  cmd.Stdout = os.Stdout
+  cmd.Run()
+}
+// TODO: 
+/*
+GET   /users/<user>
+GET   /tasks/<user>
+GET   /results/<user>
+POST  /tasks/<user>
+*/
+func getUsers(Link string){
+  resp, err := http.Get(Link+"/users")
+  if err != nil {
+    fmt.Println("Error sending request")
+    return
+  }
+  defer resp.Body.Close()
+  
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    fmt.Println("Error reading message")
+    return
+  }
+  fmt.Println(string(body))
+}
+
+func addTask(url string, name string, id int, arg1 string, arg2 string) {
+  endp := url + "/tasks/" + name
+  
+  // Define a struct to represent the task data
+  type task struct {
+  	Name string `json:"name"`
+  	ID   int    `json:"id"`
+  	Arg1 string `json:"arg1"`
+  	Arg2 string `json:"arg2"`
+  }
+  
+  // Create a task instance with the provided arguments
+  taskData := task{
+  	Name: name,
+  	ID:   id,
+  	Arg1: arg1,
+  	Arg2: arg2,
+  }
+  
+  // Marshal the task struct into JSON
+  data, err := json.Marshal(taskData)
+  if err != nil {
+  	fmt.Println("Error marshaling JSON:", err)
+  	return
+  }
+  
+  // Send a POST request with the JSON data
+  resp, err := http.Post(endp, "application/json", bytes.NewBuffer(data))
+  if err != nil {
+  	fmt.Println("Error sending request:", err)
+  	return
+  }
+  defer resp.Body.Close()
+  
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+  	fmt.Println("Error reading response:", err)
+  	return
+  }
+  
+  fmt.Println(string(body))
+}
 
 func main() {
-  fmt.Println("Hello World")
+  var IP string
+  var Port string
+  var Option int
+  var Whatever rune
+  // for AddTask 
+  var name string
+  var id int
+  var arg1 string
+  var arg2 string
+  // just the link...
+  Link := "http://"
+  
+  fmt.Println("---------[ Command & Control Client ]---------")
+  
+  fmt.Print("Server IP: ")
+  fmt.Scanln(&IP)
+  fmt.Print("Server Port: ")
+  fmt.Scanln(&Port)
+  
+  Link += IP + ":" + Port
+  for ;; {
+    clears();
+    fmt.Println("1 > List all users")               // WORKS
+    fmt.Println("2 > Show specifc user")
+    fmt.Println("3 > Show results of specific user")
+    fmt.Println("4 > Add new task to queue")        // WORKS
+    fmt.Println("5 > Show tasks in queue\n")
+    fmt.Print("Please pick an option\n > ")
+    fmt.Scanln(&Option)
+    switch Option {
+    case 1:
+      getUsers(Link)
+    case 2:
+    case 3:
+    case 4:
+      fmt.Print(" > Username: ")
+      fmt.Scanln(&name)
+      // TODO: ADD MENU WITH EACH CORRESPONDING ID
+      fmt.Print(" > ID: ")
+      fmt.Scanln(&id)
+      fmt.Print(" > 1st Argument (Opt.): ")
+      fmt.Scanln(&arg1)
+      fmt.Print(" > 2nd Argument (Opt.): ")
+      fmt.Scanln(&arg2)
+      addTask(Link, name, id, arg1, arg2)
+    case 5:
+    default:
+      fmt.Println("[x] Invalid Option!")
+    }
+    fmt.Scanln(&Whatever)
+  }
 }
